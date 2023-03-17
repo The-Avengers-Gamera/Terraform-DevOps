7db2f282834cb6c2f2dbe4b0b62b655d6e634dd6
Ueovl <yyaug99@gmail.com>
2023-03-17 20:14:53 +1100

    Fix bugs cause database cannot be deleted and ecr repository will get delete when using prod env

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -1,7 +1,7 @@
 resource "aws_ecr_repository" "gamera-ecr" {
   count = var.environment == "prod" ? 2 : 1
 
-  name                 = var.environment == "prod" ? "prod-gamera-repo" : "dev-gamera-repo"
+  name                 = count.index == 0 ? "dev-gamera-repo" : "prod-gamera-repo"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,15 +1,17 @@
 resource "aws_db_instance" "gamera-postgres-db" {
   count = var.environment == "prod" ? 2 : 1
 
-  db_name           = "${count.index == 0 ? "dev" : "prod"}-gamera-db"
+  db_name           = "${count.index == 0 ? "dev" : "prod"}_gamera_db"
   engine            = "postgres"
   allocated_storage = count.index == 0 ? 10 : 50
-  instance_class    = count.index == 0 ? "db.t2.micro" : "db.t3.medium"
+  instance_class    = count.index == 0 ? "db.t3.micro" : "db.t3.medium"
   storage_type      = "gp2"
 
   username             = "postgres"
   password             = random_password.rds-random-password[count.index].result
-  parameter_group_name = "default.postgres10"
+  parameter_group_name = "default.postgres14"
+  skip_final_snapshot       = false
+  final_snapshot_identifier = "${count.index == 0 ? "dev" : "prod"}-gamera-db-final-snapshot"
   publicly_accessible  = count.index == 0
 
   vpc_security_group_ids = [
