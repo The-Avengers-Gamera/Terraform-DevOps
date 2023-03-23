329ad40bab8d9d7dd0ba330a7a02e511a8186bff
Ueovl <yyaug99@gmail.com>
2023-03-23 18:35:14 +1100

    Change the deprecated part

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,10 +1,25 @@
 resource "aws_s3_bucket" "website-host-bucket" {
   bucket = var.website-bucket-name
-  acl    = "public-read"
-  policy = templatefile("${path.module}/policy.json", { bucket-name = var.website-bucket-name })
+}
 
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
+resource "aws_s3_bucket_acl" "website-host-bucket-acl" {
+  bucket = aws_s3_bucket.website-host-bucket.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+  bucket = aws_s3_bucket.website-host-bucket.id
+  policy = templatefile("${path.module}/policy.json", { bucket-name = var.website-bucket-name })
+}
+
+resource "aws_s3_bucket_website_configuration" "website-configuration" {
+  bucket = aws_s3_bucket.website-host-bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
   }
 }
