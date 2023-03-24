e67f59c94d2103f634799f1a0be875ff0bf90b21
Ueovl <yyaug99@gmail.com>
2023-03-24 21:12:43 +1100

    Remove redundant part

--- modules/cloudfront/variables.tf
+++ modules/cloudfront/variables.tf
@@ -6,8 +6,3 @@ variable "environment" {
 variable "gamera-website-host-buckets" {
   description = "This is the list of gamera's website host buckets"
 }
-
-/*variable "acm-certificate-arn" {
-  type = string
-  description = "This is the ACM's certificate's arn"
-}*/
